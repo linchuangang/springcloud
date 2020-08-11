2635ce56ec15ccaf68f2ad63aec56654549ab018
lcg <chuangang.lin@inrevo.io>
2020-08-11 19:07:17 +0800

    增加 Hystrix 逻辑

--- user/src/main/java/com/lin/user/controller/UserController.java
+++ user/src/main/java/com/lin/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.lin.user.controller;
 
 
 import com.lin.user.service.OrderFeign;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class UserController {
     @Autowired
     private OrderFeign orderFeign;
 
+    @HystrixCommand(fallbackMethod = "error")
     @RequestMapping("/userTest")
     public String memTest() {
         String str = restTemplate.getForObject("http://order-server/orderTest", String.class);
@@ -26,6 +28,10 @@ public class UserController {
     public String userTest2(){
         String str = orderFeign.orderTest2();
         return str;
+    }
+
+    public String error(){
+        return "服务繁忙，请稍后访问";
     }
 
 
